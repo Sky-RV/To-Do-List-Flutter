2bac577444db201f64c7e1d465829726e5e286dc
Sky <Sky.Rivera89@gmail.com>
2024-04-17 21:41:18 +0330

    Install new packages (which won't work)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutterproject/pages/home_page.dart';
+//import 'package:hive_flutter/hive_flutter.dart';
 
 void main() {
+
+  // init the hive
+  //Hive.
+
   runApp(const MyApp());
 }
 
