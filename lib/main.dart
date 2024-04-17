11c9e404117bd33e590a616ad5694657f0e9268b
Sky <Sky.Rivera89@gmail.com>
2024-04-17 19:55:52 +0330

    Create tasks and Show them

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterproject/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,40 +7,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        centerTitle: true,
-        title: Text("To Do List", style: TextStyle(
-          color: Colors.black
-        ),),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        backgroundColor: Colors.amber,
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.amber // appbar color in all pages (not working on chrome emulator)
       ),
     );
   }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutterproject/util/dialog_box.dart';
+import 'package:flutterproject/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  // text controller
+  final _controller = TextEditingController();
+
+  // list of to do tasks
+  List toDoList = [
+    ["Task Name", false],
+    ["Task Name 1", false],
+    ["Task Name 2", false],
+  ];
+
+  // checkbox was tapped
+  void checkBoxChanged(bool? value, int index){
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  // save a task
+  void saveNewTask(){
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+
+  // create a new task
+  void createNewTask(){
+    showDialog(
+        context: context,
+        builder: (context){
+          return DialogBox(
+            controller: _controller,
+            onSave: saveNewTask,
+            onCancel: () => Navigator.of(context).pop(),
+          );
+        }
+    );
+  }
+
+  // // delete task
+  // void deleteTask(int index){
+  //   setState(() {
+  //     toDoList.removeAt(index);
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      appBar: AppBar(
+        title: Text("To Do"),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+        elevation: 0, // remove app bar shadow
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.amber,
+      ),
+
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index){
+          return ToDoTile(
+              taskName: toDoList[index][0],
+              taskCompleted: toDoList[index][1],
+              onChanged: (value) => checkBoxChanged(value, index),
+            // deleteFunction: (context) => deleteTask(index),
+          );
+        },
+      ),
+
+      // body: ListView(
+      //   children: [
+      //     ToDoTile(
+      //       taskName: "Make Tutorial",
+      //       taskCompleted: true,
+      //       onChanged: (p0){}, // do nothing right now
+      //     ),
+      //     ToDoTile(
+      //       taskName: "Do Exercise",
+      //       taskCompleted: false,
+      //       onChanged: (p0){}, // do nothing right now
+      //     ),
+      //   ],
+      // ),
+    );
+  }
+}
